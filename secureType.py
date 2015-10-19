8019dcc20c92cb147d94d468ab1599fdcaaec032
Anas Syed <anas.syed@arm.com>
2015-10-19 22:50:50 +0100

    Removed unnecessary import os and only read first line of stdin

--- secureType.py
+++ secureType.py
@@ -2,13 +2,12 @@
 import subprocess
 import sys
 from time import sleep
-import os
 
 if len(sys.argv) == 2:
     delay = int(sys.argv[1])
 else:
     delay = 3
 
-data = sys.stdin.read().rstrip('\n')
+data = sys.stdin.readline().rstrip('\n')
 sleep(delay)
 subprocess.call(['xdotool', 'type', '--clearmodifiers', data])
