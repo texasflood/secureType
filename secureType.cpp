536286a9ffdeacba08e82cecab72d6804a97f168
Anas Syed <anas.syed@arm.com>
2016-01-29 12:25:20 +0000

    More error checking on sleep value argument

--- secureType.cpp
+++ secureType.cpp
@@ -3,7 +3,8 @@ extern "C" {
 }
 #include <string>
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     std::string input;
@@ -13,7 +14,15 @@ int main(int argc, char *argv[]) {
     if (argc < 2) {
         sleep(3);
     } else {
-        sleep(atoi(argv[1]));
+        char* endptr;
+        unsigned long value = strtoul(argv[1], &endptr, 10);
+        if (endptr != argv[1] && *endptr == '\0') {
+            sleep(value);
+        } else {
+            std::cout << "Failed to convert " << argv[1] << " to an "
+                    "integer\n";
+            return -1;
+        }
     }
 
     xdo_enter_text_window(x, CURRENTWINDOW, input.c_str(), 12000);
